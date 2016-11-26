afbdb87fb6d26bbafcf3b3fe471f66df81928ca4
lucaswadedavis <luke@patreon.com>
2016-11-25 23:38:07 -0800

    Some progress. RSS and Audio File downloads. Some problems too...

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Oddpod
 
 So, I'm trying to mashup a Feeldy-Saved-For-Later-to-Podcast thing. IFTTT's in the middle sending the saved for later stuff one thing at a time to this little node app which will retrieve the HTML and clean it up with `unfluff.js`, send it to Watson which will send back an audio file which I'll then insert into a podcast RSS feed for myself.
+
+I'm trying to test it locally, exposing the RSS feed and audio files through localtunnel until I can verify that everything's working well.
+
+So far it's mostly working, except for the very last mile. I can generate the audio files and RSS feed and subscribe with my Podcast App (Podcast Addict) but it errors out after downloading the (appropriately sized) OGG file, saying something about it not being the right file format...
--- index.js
+++ index.js
@@ -2,17 +2,36 @@ var port = process.argv[2] || process.env.PORT || 8080;
 
 var express = require('express');
 var jsonfile = require('jsonfile');
+var humps = require('humps');
 var extractor = require('unfluff');
 var got = require('got');
-var join = require('path').join;
+var fs = require('fs');
+var removeWhitespace = require('remove-whitespace');
+var watson = require('watson-developer-cloud');
+var Podcast = require('podcast');
+var config = require('./config.js');
 
+var join = require('path').join;
 var app = express();
 var filePath = './data.json';
-var webClient = join(__dirname, 'web-client');
-app.use(express.static(webClient));
+//var host = 'https://eiwzgndtuq.localtunnel.me/';
+var host = 'https://ofibdjeqfy.localtunnel.me/';
+
+
+
+var www = join(__dirname, 'audio');
+app.use(express.static(www));
+
+
+var text_to_speech = watson.text_to_speech({
+  username: config.ibm.username,
+  password: config.ibm.password,
+  version: 'v1'
+});
+
 app.get('/url', (req, res) => {
-    console.log(req.body);
-    var url = 'https://www.bloomberg.com/view/articles/2016-11-23/fake-news-may-not-be-protected-speech';
+    //var url = 'https://www.bloomberg.com/view/articles/2016-11-23/fake-news-may-not-be-protected-speech';
+    var url = 'https://www.scientificamerican.com/article/hidden-ldquo-planet-x-rdquo-could-orbit-in-outer-solar-system/';
     var data = {}
     jsonfile.writeFileSync(filePath, data);
     got(url)
@@ -23,14 +42,46 @@ app.get('/url', (req, res) => {
         return urlData;
     })
     .then(urlData => {
-        var audioFilePath = urlData.title;
-        // then we send it to Watson
-        // watson will return an audio file?
-        // we'll save the audio file to the audio folder?
-        return audioFilePath;
-    })
-    .then(audioFilePath => {
-        // then we add the relevant data to the rss feed.
+        var audioFilePath = encodeURI(removeWhitespace(humps.decamelize(urlData.title))) + '.ogg';
+        urlData.audioFilePath = audioFilePath;
+        console.log(urlData.title);
+        urlData.text;
+        var bytes = Buffer.byteLength(urlData.text, 'utf8');
+        var text = urlData.text.split('.');
+        var str = '';
+        for (var i = 0, str = ''; Buffer.byteLength(str) + Buffer.byteLength(text[i]) < 5000; i++) {
+            str += text[i];
+        }
+        console.log('final str size: ' + Buffer.byteLength(str));
+        var params = {
+          //text: str,
+          text: str,
+          voice: 'en-US_AllisonVoice',
+          accept: 'audio/ogg;codecs=opus'
+          //accept: 'audio/wav'
+        };
+
+        // Pipe the synthesized text to a file.
+        text_to_speech
+            .synthesize(params, function (req, res) {
+                fs.writeFile('./audio/' + audioFilePath, res, function () {console.log('done writing');});
+                // then we add the relevant data to the rss feed.
+                var feed = new Podcast({title: 'oddpod'});
+                console.log('at the feed step.');
+
+                feed.item({
+                    title: urlData.title,
+                    enclosure: {
+                        url: host + urlData.audioFilePath,
+                        file: './audio/' + urlData.audioFilePath
+                    }
+                })
+
+                fs.writeFileSync('./audio/feed.rss', feed.xml());
+            });
+            //.pipe(fs.createWriteStream('./audio/' + audioFilePath));
+
+        return urlData;
     })
     .catch(error => {
         console.log(error.response.body);
